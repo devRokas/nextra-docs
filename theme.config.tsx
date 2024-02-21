3992d6e72fb79296c561049a73a26413ddd9ea2d
Rokas Dambrauskas <47928198+devRokas@users.noreply.github.com>
2024-02-21 11:19:24 +0200

    Add files via upload

---
+++ theme.config.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>Enterprise ZK API</span>,
+  footer: {
+    text: 'Nextra Docs Template',
+  },
+  feedback: {
+    content: null
+  },
+  editLink: {
+    component: null
+  }
+}
+
+export default config
